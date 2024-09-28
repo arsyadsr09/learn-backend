a162c7bde4ef5285b9a434da112f17a97c9691d6
Arsyad Ramadhan <blacklistdark1987@gmail.com>
2024-09-28 18:15:39 +0700

    try update no middleware get get absences

--- src/routes/absence.routes.js
+++ src/routes/absence.routes.js
@@ -5,7 +5,7 @@ import absenceController from '../controllers/absence.controller'
 const absencesRoutes = Router()
 
 absencesRoutes.post('/', JwtService.jwtGetToken, absenceController.add)
-absencesRoutes.get('/', JwtService.jwtGetToken, absenceController.getAll)
+absencesRoutes.get('/', absenceController.getAll)
 absencesRoutes.put('/:absence_id', JwtService.jwtGetToken, absenceController.update)
 
 export { absencesRoutes }
