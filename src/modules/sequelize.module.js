1cc841453440ed2d4c58f1747a85835a7221337b
Arsyad Ramadhan <blacklistdark1987@gmail.com>
2024-09-28 23:12:52 +0700

    check connection

--- src/modules/sequelize.module.js
+++ src/modules/sequelize.module.js
@@ -1,35 +1,37 @@
-import { Sequelize } from "sequelize";
-import { logger } from "express-glass";
-import databaseConfig from "../config/database";
-import fs from "fs";
+import { Sequelize } from 'sequelize'
+import { logger } from 'express-glass'
+import databaseConfig from '../config/database'
+import fs from 'fs'
 
-const modelFiles = fs
-  .readdirSync(__dirname + "/../models/")
-  .filter((file) => file.endsWith(".js"));
+const modelFiles = fs.readdirSync(__dirname + '/../models/').filter((file) => file.endsWith('.js'))
 
-export const connection = new Sequelize(databaseConfig);
+export const connection = new Sequelize(databaseConfig)
 
 const sequelizeService = {
-  init: async () => {
-    try {
-      for (const file of modelFiles) {
-        const model = await import(`../models/${file}`);
-        model.default.init(connection);
-      }
+	init: async () => {
+		try {
+			await connection.authenticate()
+			console.log('Connection has been established successfully.')
+		} catch (error) {
+			console.error('Unable to connect to the database:', error)
+		}
+		try {
+			for (const file of modelFiles) {
+				const model = await import(`../models/${file}`)
+				model.default.init(connection)
+			}
 
-      modelFiles.map(async (file) => {
-        const model = await import(`../models/${file}`);
-        model.default.associate && model.default.associate(connection.models);
-      });
+			modelFiles.map(async (file) => {
+				const model = await import(`../models/${file}`)
+				model.default.associate && model.default.associate(connection.models)
+			})
 
-      logger().info("[SEQUELIZE] Database service initialized");
-    } catch (error) {
-      logger().error(
-        "[SEQUELIZE] Error during database service initialization"
-      );
-      throw error;
-    }
-  },
-};
+			logger().info('[SEQUELIZE] Database service initialized')
+		} catch (error) {
+			logger().error('[SEQUELIZE] Error during database service initialization')
+			throw error
+		}
+	},
+}
 
-export default sequelizeService;
+export default sequelizeService
